68dd6e8089e60e392b3d14c44478ff223f532923
Jan Feitsma <jan.feitsma@asml.com>
2022-04-03 12:18:06 +0200

    major redesign: create Configuration class, it uses logging.config.dictConfig, simplify API

--- extendedlogging.py
+++ extendedlogging.py
@@ -1,19 +1,98 @@
-# -*- coding: utf-8 -*-
-
+"""Combine standard python logging with autologging/tracing."""
 __author__ = 'Jan Feitsma'
 
-import logging
-import autologging
+import sys
+import os
+import time
 import numpy
+import logging
+import logging.config
+import autologging
 
-
-# export everything combined
+# interface dealing
 from logging import *
 from autologging import *
 
-
+# constants
 ARRAY_SIZE_LIMIT = None # TODO? not implemented.
 STRING_SIZE_LIMIT = None
+DEFAULT_LOG_FILE = '/tmp/extendedlogging.txt'
+BASIC_FORMAT = "%(levelname)s:%(name)s:%(message)s"
+#EXTENDED_FORMATTER = ExtendedFormatter
+
+
+def configure(**kwargs):
+    # TODO: module __doc__; describe all options (or write out kwargs?)
+    c = Configuration(**kwargs)
+    c.apply()
+
+def remove_all_handlers():
+    handlers = logging.root.handlers[:]
+    for handler in handlers:
+        logging.root.removeHandler(handler)
+        handler.close()
+
+
+
+class Configuration():
+    def __init__(self, **kwargs):
+        self.name = ''
+        self.console = True # stdout
+        self.tracing = False
+        self.reset = True
+        self.filename = DEFAULT_LOG_FILE
+        self.format_stdout = '%(levelname)-7s:%(funcName)s:%(message)s'
+        self.format_file = '%(asctime)s:%(levelname)s:%(filename)s,%(lineno)d:%(name)s.%(funcName)s:%(message)s'
+        self.level_stdout = logging.INFO
+        self.level_file = autologging.TRACE
+        self.config = None
+        # set overruled options, if any
+        self.__dict__.update(kwargs)
+
+    def clear(self):
+        """Reset/clear logging configuration / handlers."""
+        remove_all_handlers()
+        # cleanup log file if existing
+        if os.path.exists(self.filename) and self.tracing:
+            os.remove(self.filename)
+
+    def apply(self):
+        """Apply the configuration."""
+        # check if autologging has been disabled (typically via environment variable)
+        if autologging.traced == autologging._traced_noop:
+            self.tracing = False
+        # reset?
+        if self.reset:
+            self.clear()
+        # configure logging
+        self.config = self.make_config_dict()
+        #print(self.config)
+        logging.config.dictConfig(self.config)
+        return logging.getLogger(self.name)
+
+    def make_config_dict(self):
+        result = {
+            'version': 1,
+            'formatters': {},
+            'handlers': {},
+            'loggers': {
+                self.name: {
+                    'level': autologging.TRACE,
+                    'handlers': []
+                },
+            },
+        }
+        if self.console:
+            result['formatters']['logformatter'] = {'format': self.format_stdout}
+            result['handlers']['loghandler'] = {'class': 'logging.StreamHandler', 'stream': sys.stdout, 'level': self.level_stdout, 'formatter': 'logformatter'}
+            result['loggers'][self.name]['handlers'].append('loghandler')
+        if self.tracing:
+            result['formatters']['traceformatter'] = {'format': self.format_file}
+            result['handlers']['tracehandler'] = {'class': 'logging.FileHandler', 'level': self.level_file, 'formatter': 'traceformatter', 'filename': self.filename}
+            result['loggers'][self.name]['handlers'].append('tracehandler')
+        return result
+
+
 
 class ExtendedFormatter(logging.Formatter):
     def format(self, record):
@@ -48,8 +127,6 @@ class ExtendedFormatter(logging.Formatter):
         return result_string
 
 
-BASIC_FORMAT = "%(levelname)s:%(name)s:%(message)s"
-EXTENDED_FORMATTER = ExtendedFormatter
 
 
 def extendedConfig(**kwargs):
